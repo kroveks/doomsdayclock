ff2f51e5e875f2b54388496a26f1cb2f5d72aeee
nehor <nehoroshkovslava@gmail.com>
2020-12-08 15:55:37 +0300

    First commit epta

---
+++ src/test/java/com/ddclock/doomsday/DoomsdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddclock.doomsday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoomsdayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
